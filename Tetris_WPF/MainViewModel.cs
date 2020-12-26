9b72c1610e59c2fd85e67b84a1a3d904261d6008
Daniel Bhatti <danielbbhatti@gmail.com>
2020-12-25 20:34:27 -0500

    Added a Tetris_WPF project, going to try to recreate Tetris via WPF.

---
+++ Tetris_WPF/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tetris_WPF
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public string BindingTestString { get; set; }
+
+        public MainViewModel()
+        {
+            BindingTestString = "Test Value";
+        }
+    }
+}
---
+++ Tetris_WPF/ViewModelLocator.cs
@@ -0,0 +1,29 @@
+﻿/*
+  In App.xaml:
+  <Application.Resources>
+      <vm:ViewModelLocator xmlns:vm="clr-namespace:ImportExport"
+                           x:Key="Locator" />
+  </Application.Resources>
+  
+  In the View:
+  DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
+  You can also use Blend to do all this with the tool's support.
+  See http://www.galasoft.ch/mvvm
+*/
+
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using CommonServiceLocator;
+//using Microsoft.Practices.ServiceLocation;
+
+namespace Tetris_WPF
+{
+    /// <summary>
+    /// This class contains static references to all the view models in the
+    /// application and provides an entry point for the bindings.
+    /// </summary>
+    public class ViewModelLocator
+    {
+        public ViewModelLocator() { }
+    }
+}
